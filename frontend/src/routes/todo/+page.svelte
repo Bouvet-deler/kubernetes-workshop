f4cdeb978519453966e081afffd90c6ea3a66336
alexmstaff <alexander.staff@bouvet.no>
2024-02-14 07:53:51 +0100

    Add frontend files

---
+++ frontend/src/routes/todo/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let data: string;
+
+  onMount(async () => {
+    const resp = await fetch('/api/todo');
+    console.log(resp);
+    data = await resp.json();
+  });
+</script>
+
+TODO:
+
+{JSON.stringify(data)}
