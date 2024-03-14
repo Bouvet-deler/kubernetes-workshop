576c45b51eae49e6defe63f7e1b2623bded2e5dd
alexmstaff <alexander.staff@bouvet.no>
2024-03-14 10:54:34 +0100

    Add some styling

--- frontend/src/routes/todo/+page.svelte
+++ frontend/src/routes/todo/+page.svelte
@@ -78,13 +78,27 @@
 			>
 				<input type="checkbox" on:click={() => toggleTask(todo)} checked={todo.completed} />
 				<div>{todo.message}</div>
-				<input type="button" value="Delete" on:click={() => deleteTodo(todo)} />
+				<input
+					type="button"
+					class="button delete"
+					value="Delete"
+					on:click={() => deleteTodo(todo)}
+				/>
 			</div>
 		{/each}
 
 		<div class="flex">
-			<input type="text" bind:value={newMessage} />
-			<input type="button" value="Create todo" on:click={() => createTodo()} />
+			<input
+				type="text"
+				style="width: 100%; border: 2px slategray solid;"
+				bind:value={newMessage}
+			/>
+			<input
+				type="button"
+				class="button create"
+				value="Create todo"
+				on:click={() => createTodo()}
+			/>
 		</div>
 	{:else}
 		{JSON.stringify(todos)}
@@ -105,5 +119,29 @@
 	.parent-container {
 		border: 2px slategray solid;
 		gap: 1rem;
+	}
+
+	.create {
+		background-color: #04aa6d;
+	}
+
+	.delete {
+		background-color: #f44336;
+	}
+
+	.button {
+		border: none;
+		color: white;
+		padding: 15px 32px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-size: 16px;
+	}
+
+	.button:hover {
+		box-shadow:
+			0 12px 16px 0 rgba(0, 0, 0, 0.24),
+			0 17px 50px 0 rgba(0, 0, 0, 0.19);
 	}
 </style>
